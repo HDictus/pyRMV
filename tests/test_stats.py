5e21d8dca293331af91d07f2bc7275aa0f6128db
Dictus Hugo <hugo.dictus@epfl.ch>
2023-03-21 14:13:33 +0100

    remove redundant import

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 import pytest as pyt
 import scipy
-import statsmodels.stats.proportion as proportion
 from analysis_neuro import stats, terms, Assumption
 
 
