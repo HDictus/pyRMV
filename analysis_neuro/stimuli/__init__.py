74d1947ada1aed59b0f930ae33e5ef03412bb7e2
hugs <h.t.dictus@gmail.com>
2023-08-17 16:52:52 +0200

    remove redundant stimuli.get method

--- analysis_neuro/stimuli/__init__.py
+++ analysis_neuro/stimuli/__init__.py
@@ -10,18 +10,6 @@ from analysis_neuro import terms
 from . import allen_brain_observatory
 
 
-# TODO: this isn't quite accurate
 stimuli = {
     **allen_brain_observatory.stimuli
 }
-
-
-# is this method redundant?
-def get(parameters):
-    """Create a dataframe of all stimulus conditions for STIMULUS."""
-    out = []
-    for i, row in parameters.iterrows():
-        all_stimuli = stimuli[row[terms.STIMULUS]]
-        other_vars = [c for c in parameters.columns if c not in all_stimuli]
-        out.append(all_stimuli.assign(**row[other_vars]))
-    return pd.concat(out)
